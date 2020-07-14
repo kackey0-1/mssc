ee0ec6ca79bd1b255bef71d155bd5f13c15f95c0
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-07-15 08:37:09 +0900

    setup

---
+++ mssc/src/main/java/com/mkpartner/mssc/MsscApplication.java
@@ -0,0 +1,13 @@
+package com.mkpartner.mssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscApplication.class, args);
+	}
+
+}
---
+++ mssc/src/test/java/com/mkpartner/mssc/MsscApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mkpartner.mssc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsscApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
