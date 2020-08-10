83261bf9995e432360cfb84edb2f8f1f9fddbaf9
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2020-08-11 07:09:26 +0900

    adding bootstrap - closes #10

---
+++ src/main/java/com/mkpartner/mssc/bootstrap/BeerLoader.java
@@ -0,0 +1,45 @@
+package com.mkpartner.mssc.bootstrap;
+
+import com.mkpartner.mssc.domain.Beer;
+import com.mkpartner.mssc.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.97"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cats")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("12.97"))
+                    .build());
+        }
+    }
+}
